1818bffebcf652eeda0b9bbd3e981d9df54d797d
Juliartwn <juliartawan630@gmail.com>
2026-02-17 09:26:22 +0800

    fix: Update minimum SDK version to 26 for TensorFlow Lite compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.corly"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24 // Required for TensorFlow Lite
+        minSdk = 26 // Required for TensorFlow Lite
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
